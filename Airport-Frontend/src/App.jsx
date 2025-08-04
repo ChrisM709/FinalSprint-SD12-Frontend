c168b030044f9428a73d00dada9fa36694c45d9c
Chris M <chrism709@users.noreply.github.com>
2025-08-04 15:34:52 -0300

    Updated React - Added react router dom for page routing

--- Airport-Frontend/src/App.jsx
+++ Airport-Frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      <h1>Home Page</h1>
+    <div className="app-container">
+      <div className="card">
+        <h1>✈️ Flight Tracker</h1>
+        <div className="button-group">
+          <button className="btn">View Flights</button>
+          <button className="btn">Admin Dashboard</button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
